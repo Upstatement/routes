cb8edd93c64260b304fd4095b8356055fa7534cd
Jared Novack <jared@upstatement.com>
2015-02-16 14:53:23 -0800

    Added tests from Timber and using PHPRouter instead of AltoRouter

--- routes.php
+++ routes.php
@@ -1,120 +1,132 @@
 <?php
 /*
 Plugin Name: Routes
-Plugin URI: http://timber.upstatement.com
+Plugin URI: http://routes.upstatement.com
 Description: The WordPress Timber Library allows you to write themes using the power Twig templates
 Author: Jared Novack + Upstatement
 Version: 0.1
 Author URI: http://upstatement.com/
-*/
 
-//Routes::map('/products/sales', 'page#sales');
-//Routes::map('')
+Usage:
 
-/*
-$router = new AltoRouter();
-
-// map homepage
-$router->map( 'GET', '/', function() {
-    require __DIR__ . '/views/home.php';
-});
-
-// map users details page
-$router->map( 'GET|POST', '/users/[i:id]/', function( $id ) {
-  $user = .....
-  require __DIR__ . '/views/user/details.php';
+Routes::map('/my-location', function(){
+	//do stuff
+	Routes::load('single.php', $data);
 });
 */
+
 class Routes {
 
-	function __construct() {
-		require_once 'vendor/altorouter/altorouter/AltoRouter.php';
-		global $routes_alto_router;
-		$routes_alto_router = new AltoRouter();
-		add_action( 'init', array( $this, 'init' ) );
+	protected $router;
+
+	function __construct(){
+		add_action('init', array($this, 'init'));
 	}
 
 	function init() {
-		global $routes_alto_router;
-		if ( isset( $routes_alto_router ) ) {
-			$match = $routes_alto_router->match();
-		}
-		if ( isset( $match ) && $match ) {
-			$callback = $match['target'];
-			$params = $match['params'];
-			call_user_func_array( $callback, $params );
-		}
-	}
+        global $upstatement_routes;
+        if (isset($upstatement_routes->router)) {
+            $route = $upstatement_routes->router->matchCurrentRequest();
+            if ($route) {
+                $callback = $route->getTarget();
+                $params = $route->getParameters();
+                $callback($params);
+            }
+        }
+    }
 
-	public static function map( $route, $callback ) {
-		global $routes_alto_router;
-		if ( isset( $routes_alto_router ) ) {
-			$routes_alto_router->map( 'GET', $route, $callback );
-		}
-	}
+    /**
+     * @param string $route
+     * @param callable $callback
+     */
+    public static function map($route, $callback, $args = array()) {
+        global $upstatement_routes;
+        if (!isset($upstatement_routes->router)) {
+            if (class_exists('PHPRouter\Router')){
+                $upstatement_routes->router = new PHPRouter\Router();
+                $site_url = get_bloginfo('url');
+                $site_url_parts = explode('/', $site_url);
+                $site_url_parts = array_slice($site_url_parts, 3);
+                $base_path = implode('/', $site_url_parts);
+                if (!$base_path || strpos($route, $base_path) === 0) {
+                    $base_path = '/';
+                } else {
+                    $base_path = '/' . $base_path . '/';
+                }
+                $upstatement_routes->router->setBasePath($base_path);
+            }
+        }
+        if (class_exists('PHPRouter\Router')){
+            $upstatement_routes->router->map($route, $callback, $args);
+        }
+    }
 
-	/**
-	 *
-	 *
-	 * @param array   $template
-	 * @param mixed   $query
-	 * @param int     $status_code
-	 * @param bool    $tparams
-	 * @return bool
-	 */
-	public static function load_view( $template, $query = false, $status_code = 200, $tparams = false ) {
-		$fullPath = is_readable( $template );
-		if ( !$fullPath ) {
-			$template = locate_template( $template );
-		}
-		if ( $tparams ) {
-			global $params;
-			$params = $tparams;
-		}
-		if ( $status_code ) {
-			add_filter( 'status_header', function( $status_header, $header, $text, $protocol ) use ( $status_code ) {
-					$text = get_status_header_desc( $status_code );
-					$header_string = "$protocol $status_code $text";
-					return $header_string;
-				}, 10, 4 );
-			if ( 404 != $status_code ) {
-				add_action( 'parse_query', function( $query ) {
-						if ( $query->is_main_query() ) {
-							$query->is_404 = false;
-						}
-					}, 1 );
-				add_action( 'template_redirect', function() {
-						global $wp_query;
-						$wp_query->is_404 = false;
-					}, 1 );
-			}
-		}
+    /**
+     * @param array $template
+     * @param mixed $query
+     * @param int $status_code
+     * @param bool $tparams
+     * @return bool
+     */
+    public static function load($template, $query = false, $status_code = 200, $tparams = false) {
+        $fullPath = is_readable($template);
+        if (!$fullPath) {
+            $template = locate_template($template);
+        }
+        if ($tparams){
+            global $params;
+            $params = $tparams;
+        }
+        if ($status_code) {
+            add_filter('status_header', function($status_header, $header, $text, $protocol) use ($status_code) {
+                $text = get_status_header_desc($status_code);
+                $header_string = "$protocol $status_code $text";
+                return $header_string;
+            }, 10, 4 );
+            if (404 != $status_code) {
+                add_action('parse_query', function($query) {
+                    if ($query->is_main_query()){
+                        $query->is_404 = false;
+                    }
+                },1);
+                add_action('template_redirect', function(){
+                    global $wp_query;
+                    $wp_query->is_404 = false;
+                },1);
+            }
+        }
 
-		if ( $query ) {
-			add_action( 'do_parse_request', function() use ( $query ) {
-					global $wp;
-					if ( is_callable( $query ) ) {
-						$query = call_user_func( $query );
-					}
-					if ( is_array( $query ) ) {
-						$wp->query_vars = $query;
-					} elseif ( !empty( $query ) ) {
-						parse_str( $query, $wp->query_vars );
-					} else {
-						return true; // Could not interpret query. Let WP try.
-					}
-					return false;
-				} );
-		}
-		if ( $template ) {
-			add_filter( 'template_include', function( $t ) use ( $template ) {
-					return $template;
-				} );
-			return true;
-		}
-		return false;
-	}
+        if ($query) {
+            add_action('do_parse_request', function() use ($query) {
+                global $wp;
+                if ( is_callable($query) )
+                    $query = call_user_func($query);
 
+                if ( is_array($query) )
+                    $wp->query_vars = $query;
+                elseif ( !empty($query) )
+                    parse_str($query, $wp->query_vars);
+                else
+                    return true; // Could not interpret query. Let WP try.
+
+                return false;
+            });
+        }
+        if ($template) {
+        	add_filter('template_include', function($t) use ($template) {
+        		return $template;
+        	});
+            return true;
+        }
+        return false;
+    }
 }
 
-new Routes();
+global $upstatement_routes;
+$upstatement_routes = new Routes();
+
+if (    file_exists($composer_autoload = __DIR__ . '/vendor/autoload.php')
+        || file_exists($composer_autoload = WP_CONTENT_DIR.'/vendor/autoload.php')){
+  require_once($composer_autoload);
+}
+
---
+++ tests/test-routes.php
@@ -0,0 +1,80 @@
+<?php
+
+class TestRoutes extends WP_UnitTestCase {
+
+	function testThemeRoute(){
+		$template = Routes::load('single.php');
+		$this->assertTrue($template);
+	}
+
+	function testThemeRouteDoesntExist(){
+		$template = Routes::load('singlefoo.php');
+		$this->assertFalse($template);
+	}
+
+	function testFullPathRoute(){
+		$hello = WP_CONTENT_DIR.'/plugins/hello.php';
+		$template = Routes::load($hello);
+		$this->assertTrue($template);
+	}
+
+	function testFullPathRouteDoesntExist(){
+		$hello = WP_CONTENT_DIR.'/plugins/hello-foo.php';
+		$template = Routes::load($hello);
+		$this->assertFalse($template);
+	}
+
+	function testRouterClass(){
+		$this->assertTrue(class_exists('PHPRouter\Router'));
+	}
+
+	function testAppliedRoute(){
+		$_SERVER['REQUEST_METHOD'] = 'GET';
+		global $matches;
+		$matches = array();
+		$phpunit = $this;
+		Routes::map('foo', function() use ($phpunit) {
+			global $matches;
+			$phpunit->assertTrue(true);
+			$matches[] = true;
+		});
+		$this->go_to(home_url('foo'));
+		$this->matchRoutes();
+		$this->assertEquals(1, count($matches));
+	}
+
+	function testRouteAgainstPostName(){
+		$post_name = 'jared';
+		$post = $this->factory->post->create(array('post_title' => 'Jared', 'post_name' => $post_name));
+		global $matches;
+		$matches = array();
+		$phpunit = $this;
+		Routes::map('randomthing/'.$post_name, function() use ($phpunit) {
+			global $matches;
+			$phpunit->assertTrue(true);
+			$matches[] = true; 
+		});
+		$this->go_to(home_url('/randomthing/'.$post_name));
+		$this->matchRoutes();
+		$this->assertEquals(1, count($matches));
+	}
+
+	function testFailedRoute(){
+		$_SERVER['REQUEST_METHOD'] = 'GET';
+		global $matches;
+		$matches = array();
+		$phpunit = $this;
+		Routes::map('foo', function() use ($phpunit){
+			$phpunit->assertTrue(false);
+			$matches[] = true;
+		});
+		$this->go_to(home_url('bar'));
+		$this->matchRoutes();
+		$this->assertEquals(0, count($matches));
+	}
+
+	function matchRoutes() {
+		global $upstatement_routes;
+		$upstatement_routes->init();
+	}
+}
